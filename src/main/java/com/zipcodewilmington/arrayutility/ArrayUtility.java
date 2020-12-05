959390758ef805dadfbffc19cc5cd581120fcf79
nikki <nikki@Gbrads-Mac.local>
2020-12-04 21:45:58 -0500

    complete

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -2,8 +2,9 @@ package com.zipcodewilmington.arrayutility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by leon on 3/6/18.
@@ -32,11 +33,29 @@ public class ArrayUtility<T> {
     }
 
     public T[] mergeArray(T[] arrayToMerge){
-        List list = new ArrayList<T>(Arrays.asList(array));
-        list.addAll(Arrays.asList(arrayToMerge));
-        T[] merged = (T[]) list.toArray();
-        return merged;
+//        List list = new ArrayList(Arrays.asList(array));
+//        list.addAll(Arrays.asList(arrayToMerge));
+//        T[] merged = (T[]) list.toArray();
+//        return merged;
+
+//        T[] result = T[array.length + arrayToMerge.length];
+//    final List<T> merged = new ArrayList<>();
+//    List<T> base = Arrays.asList(array);
+//    List<T> added = Arrays.asList(arrayToMerge);
+//    merged.addAll(base);
+//    merged.addAll(added);
+//    return (T[]) merged.toArray();
+
+        return (T[]) Stream.of(array, arrayToMerge)
+                .flatMap(Stream::of)
+                .toArray();
     }
+
+    // merge practice on int[]
+
+//    public int[] mergeIntArray(int[] arrayToMerge){
+//
+//    }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
        array = mergeArray(arrayToMerge);
@@ -61,17 +80,24 @@ public class ArrayUtility<T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        array = mergeArray(arrayToMerge);
-        HashMap<T, T> elementCountMap = new HashMap<T,T>();
-        for (T i : array){
-            Integer value = HashMap.get(i);
-
+        Integer mostCommon = Integer.MIN_VALUE;
+        List<T> arrayList = new ArrayList<>();
+        T mostCommonVal = null; //most common val doesn't exist, go thru and get it
+        arrayList.addAll(Arrays.asList(arrayToMerge));
+        arrayList.addAll(Arrays.asList(array));
+        for (T t : arrayList) {
+            Integer current =  Collections.frequency(arrayList, t);
+            if (current > mostCommon) {
+                mostCommon = current;
+                mostCommonVal = t;
+            }
         }
-        return null;
+        return mostCommonVal;
+    }
     }
 
 //        public T getMostCommonFromMerge (T[] arrayToMerge){
 //            return (T) arrayToMerge;
 //        }
 //    }
-}
+
